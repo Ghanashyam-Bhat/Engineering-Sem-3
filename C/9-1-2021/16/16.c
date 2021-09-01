9943b5d44860d0e578d344164cd9eceb335f90a8
Ghanashyam Bhat <74894094+Ghanashyam-Bhat@users.noreply.github.com>
2021-09-01 20:18:50 +0530

    Add files via upload

---
+++ C/9-1-2021/16/16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "head.h"
+
+int main()
+{
+	H h,he;
+	h.head = NULL;he.head=NULL;
+	int x,y,ans;
+	char o;
+	do
+	{
+		printf("\nSelect\n1.To initialize two polynimials\n2.To perform operations on polynomials\n");fflush(stdin);scanf("%c",&o);
+		switch(o)
+		{
+			case '1':x = initialize(&h,1);
+					 y = initialize(&he,2);
+					 printf("\nReached the Calling of merge function");
+						merge(h,he,x+y);
+						break;
+			case '2':ans = operation(&h,x);
+					printf("The answer is: %d",ans);
+						break;
+			case '3':printf("\nClosed");
+						break;
+			default:printf("\nEnter proper option");
+		}
+	}while(o!='3');
+}
